7d47838e085a5a06d6bf29b335085da1f6b70e16
MuriloAredes <murilohenrique467@gmail.com>
2022-06-13 17:00:01 -0300

    add classes

---
+++ Manager.Application/Produto/Command/Create/CreateProdutoHandler.cs
@@ -0,0 +1,49 @@
+﻿using FluentValidation;
+using Manager.Context.Repositorio.Interfaces;
+using MediatR;
+
+namespace Manager.Application.Produto.Command.Create
+{
+    public class CreateProdutoRequest : IRequest 
+    {
+        public string Name { get; set; } = string.Empty;
+        public int  Quantidade { get; set; }
+        public long CategoriaId { get; set; }
+        public double ValorUnitario { get; set; } 
+    }
+    public class CreateProdutoHandler : IRequestHandler<CreateProdutoRequest>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IValidator<CreateProdutoRequest> _validator;
+        public CreateProdutoHandler(IUnitOfWork unitOfWork,
+                                    IValidator<CreateProdutoRequest>validator)
+        {
+            _unitOfWork = unitOfWork;
+            _validator = validator;
+        }
+        public async Task<Unit> Handle(CreateProdutoRequest request, CancellationToken cancellationToken)
+        {
+            #region Validator 
+
+            var validator = _validator.Validate(request);
+
+            if (!validator.IsValid)
+                throw new Exception(string.Join(",", validator.Errors.Select(x => x.ErrorMessage)));
+            
+            #endregion
+            var produto =  _unitOfWork.Produtos.Add(new Domain.Entity.Produto
+            {
+                Name = request.Name,
+                Quantidade = request.Quantidade,
+                ValorUnitario = request.ValorUnitario,
+                CategoriaId = request.CategoriaId,
+                Ativo = true,
+                Deletado = false
+            });
+
+            await _unitOfWork.Commit();
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ Manager.Application/Validator/Produto/CheckCreateProdutoRequest.cs
@@ -0,0 +1,48 @@
+﻿using FluentValidation;
+using Manager.Application.Produto.Command.Create;
+using Manager.Context.Repositorio.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager.Application.Validator.Produto
+{
+    public class CheckCreateProdutoRequest :  AbstractValidator<CreateProdutoRequest>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CheckCreateProdutoRequest(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+
+            RuleFor(n => n.Name)
+                .NotEmpty().WithMessage("Preencha o campo nome !")
+                .NotNull().WithMessage("Preencha o campo nome ")
+                .Must(CheckHasName).WithMessage("Nome de produto já existente");
+
+            RuleFor(r => r.Quantidade)
+                .NotNull()
+                .NotEmpty()
+                .Equals(0);
+
+            RuleFor(i => i.ValorUnitario)
+                .NotEmpty()
+                .NotNull()
+                .Must(CheckMinimunValue).WithMessage("preencha o campo valor unitario");
+        }
+
+        public bool CheckHasName(string name) 
+        {
+            var produto = _unitOfWork.Produtos.Get(e => e.Name.Equals(name));
+
+            return produto != null;
+        }
+
+        public bool CheckMinimunValue(double num) 
+        {
+           return num <= 0;
+        }
+    }
+}
